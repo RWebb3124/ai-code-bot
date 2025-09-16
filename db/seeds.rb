82e2050ae45a78f46b98dba408c4b9dc81cf05a3
Ryan Webb <rwebb3124@gmail.com>
2025-09-16 16:56:44 +0100

    added challenge seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,21 @@ require 'faker'
 
 puts "cleaning database..."
 User.destroy_all
+Challenge.destroy_all
 
 puts "creating users..."
 5.times do
   first_name = Faker::Name.first_name
   User.create!(first_name: first_name , last_name: Faker::Name.last_name , username: Faker::Name.middle_name , email: Faker::Internet.email(name: first_name), password: "password" )
 end
+puts "users created!"
+
+puts "creating challenges..."
+modules = %w[Ruby Javascript CSS HTML Database]
+SYSTEM_PROMPT = "You are a Teaching Assistant. I am a beginner in web development, learning how to code. Help me break down my problem into small, actionable steps, without giving away solutions. Answer concisely in Markdown."
+x = 0
+5.times do
+  Challenge.create!(name: "#{modules[x]} basics - 01", module: modules[x], content: Faker::Lorem.paragraph(sentence_count: 4), system_prompt: SYSTEM_PROMPT)
+  x += 1
+end
+puts "finished creating challenges!"
